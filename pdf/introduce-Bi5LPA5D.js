44332bb0a22683fb37add9afa24d9c2d864b8a47
fade <30360212+fade101@users.noreply.github.com>
2025-08-27 15:12:33 +0000

    add pdf

---
+++ pdf/introduce-Bi5LPA5D.js
@@ -0,0 +1,1 @@
+import{j as e,c as t,b as r}from"./client-lPDrGpAa.js";const c=()=>e.jsx("div",{children:e.jsx("h1",{children:"模型介绍页"})});t.createRoot(document.getElementById("root")).render(e.jsx(r.StrictMode,{children:e.jsx(c,{})}));
